64ea362b0c332a23eee3c80adf82f33c3b4f837f
thisisos <68276592+thisisos@users.noreply.github.com>
2023-09-25 18:25:41 +0530

    Om Shukla Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,8 +7,13 @@ using namespace std;
 
 int main() {
     srand(time(0));
-    const int minNumber = 1;
-    const int maxNumber = 100;
+    const int minNumber = n;
+    const int maxNumber = m;
+
+    cout<<"enter min num"<<endl;
+    cin>>n;
+    cout<<"Enter Max num"<<endl;
+    cin>>m;
 
     char playAgain;
     
