a9e5a1c3fa0d528c438053bf70487e6cd98a1cb7
Vedikajawaria <144482494+Vedikajawaria@users.noreply.github.com>
2023-09-25 18:28:02 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,8 +7,10 @@ using namespace std;
 
 int main() {
     srand(time(0));
-    const int minNumber = 1;
-    const int maxNumber = 100;
+    cout<<"Enter max no.";
+    cin>>max;
+    cout<<"Enter min no.";
+    cin>>min;
 
     char playAgain;
     
